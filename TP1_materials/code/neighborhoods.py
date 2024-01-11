2c4ff0b2edebadfb63f33c7ba6ef94685244cde8
Balthazar Neveu <balthazarneveu@gmail.com>
2024-01-11 17:36:33 +0100

    #1 spherical neighbor

--- TP1_materials/code/neighborhoods.py
+++ TP1_materials/code/neighborhoods.py
@@ -33,6 +33,9 @@ from ply import write_ply, read_ply
 
 # Import time package
 import time
+from pathlib import Path
+from typing import List
+here = Path(__file__).parent
 
 
 # ------------------------------------------------------------------------------------------
@@ -45,12 +48,16 @@ import time
 #
 
 
-def brute_force_spherical(queries, supports, radius):
-
-    # YOUR CODE
-    neighborhoods = None
-
-    return neighborhoods
+def brute_force_spherical(
+        queries: np.ndarray,  # (N, 3)
+        supports: np.ndarray,  # (M, 3)
+        radius: np.ndarray) -> List[np.ndarray]:
+    # [1, N, 3] - [M, 1, 3]
+    queries = np.expand_dims(queries, axis=1)
+    supports_exp = np.expand_dims(supports, axis=0)
+    distances = np.linalg.norm(supports_exp-queries, axis=-1)
+    neighborhood_indices = np.argwhere(distances <= radius)
+    return neighborhood_indices
 
 
 def brute_force_KNN(queries, supports, k):
@@ -61,18 +68,14 @@ def brute_force_KNN(queries, supports, k):
     return neighborhoods
 
 
-
-
-
 # ------------------------------------------------------------------------------------------
 #
 #           Main
 #       \**********/
 #
-# 
+#
 #   Here you can define the instructions that are called when you execute this file
 #
-
 if __name__ == '__main__':
 
     # Load point cloud
@@ -83,7 +86,7 @@ if __name__ == '__main__':
     #
 
     # Path of the file
-    file_path = '../data/indoor_scan.ply'
+    file_path = here.parent/'data'/'indoor_scan.ply'
 
     # Load point cloud
     data = read_ply(file_path)
@@ -104,31 +107,33 @@ if __name__ == '__main__':
         num_queries = 10
 
         # Pick random queries
-        random_indices = np.random.choice(points.shape[0], num_queries, replace=False)
+        random_indices = np.random.choice(
+            points.shape[0], num_queries, replace=False)
         queries = points[random_indices, :]
 
         # Search spherical
+        total_iterations = 100
         t0 = time.time()
-        neighborhoods = brute_force_spherical(queries, points, radius)
+        for _ in range(total_iterations):
+            neighborhoods = brute_force_spherical(queries, points, radius)
         t1 = time.time()
-
-        # Search KNN      
+        # Search KNN
         neighborhoods = brute_force_KNN(queries, points, neighbors_num)
         t2 = time.time()
 
         # Print timing results
-        print('{:d} spherical neighborhoods computed in {:.3f} seconds'.format(num_queries, t1 - t0))
+        print('{:d} spherical neighborhoods computed in {:.3f} seconds'.format(
+            num_queries, (t1 - t0)/total_iterations))
         print('{:d} KNN computed in {:.3f} seconds'.format(num_queries, t2 - t1))
 
         # Time to compute all neighborhoods in the cloud
-        total_spherical_time = points.shape[0] * (t1 - t0) / num_queries
+        total_spherical_time = points.shape[0] * \
+            (t1 - t0)/total_iterations / num_queries
         total_KNN_time = points.shape[0] * (t2 - t1) / num_queries
-        print('Computing spherical neighborhoods on whole cloud : {:.0f} hours'.format(total_spherical_time / 3600))
-        print('Computing KNN on whole cloud : {:.0f} hours'.format(total_KNN_time / 3600))
-
- 
-
-
+        print('Computing spherical neighborhoods on whole cloud : {:.0f} hours'.format(
+            total_spherical_time / 3600))
+        print('Computing KNN on whole cloud : {:.0f} hours'.format(
+            total_KNN_time / 3600))
 
     # KDTree neighborhoods
     # ********************
@@ -141,7 +146,3 @@ if __name__ == '__main__':
         num_queries = 1000
 
         # YOUR CODE
-        
-        
-        
-        
