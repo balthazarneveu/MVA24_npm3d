a707777e3e851218cb6c6d87b969df91d08bb670
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-08 17:26:15 +0100

    #4 TP4 imls torch version

--- TP4_materials/code/reconstruction.py
+++ TP4_materials/code/reconstruction.py
@@ -26,7 +26,7 @@ import time
 from skimage import measure
 
 import trimesh
-
+import torch
 from tqdm import tqdm
 
 # Hoppe surface reconstruction
@@ -70,11 +70,50 @@ def compute_hoppe(points,
     out = f.reshape(grid_resolution, grid_resolution, grid_resolution)
     return out
 
-
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # IMLS surface reconstruction
-def compute_imls(points, normals, scalar_field, grid_resolution, min_grid, size_voxel, knn):
-    # YOUR CODE
-    return
+def compute_imls(points, normals, scalar_field, grid_resolution, min_grid, size_voxel, knn=30, h=0.01, device=DEVICE):
+    inv_h_square = 1/(h**2)
+    tree = KDTree(points,
+                  leaf_size=40)
+    print("done kdtree")
+    # Create a voxel grid
+    x = np.arange(grid_resolution) * size_voxel[0] + min_grid[0]
+    y = np.arange(grid_resolution) * size_voxel[1] + min_grid[1]
+    z = np.arange(grid_resolution) * size_voxel[2] + min_grid[2]
+
+    x = x[:, None, None]
+    x = np.repeat(x, repeats=grid_resolution, axis=1)
+    x = np.repeat(x, repeats=grid_resolution, axis=2)
+
+    y = y[None, :, None]
+    y = np.repeat(y, repeats=grid_resolution, axis=0)
+    y = np.repeat(y, repeats=grid_resolution, axis=2)
+
+    z = z[None, None, :]
+    z = np.repeat(z, repeats=grid_resolution, axis=0)
+    z = np.repeat(z, repeats=grid_resolution, axis=1)
+
+    xyz = np.stack((x, y, z), axis=-1).reshape(-1, 3)
+
+    cloud_id = tree.query(xyz, k=knn, return_distance=False)
+    print("done kdtree")
+    knn_points = points[cloud_id, :]
+    
+    knn_points = torch.from_numpy(knn_points).to(device)
+    knn_normals = torch.from_numpy(normals[cloud_id, :]).to(device)
+    # knn_normals = knn_normals.unsqueeze(-1)
+    print(knn_normals.shape)
+    xyz = torch.from_numpy(xyz).to(device).unsqueeze(1)
+    dist = xyz - knn_points
+    kernel = (-dist.norm(dim=-1).square()*inv_h_square).exp()
+    dot_product = (knn_normals*dist).sum(dim=-1)
+    sdf = (dot_product*kernel).sum(dim=-1)/(kernel.sum(dim=-1))
+    # print(knn_points.shape, xyz.shape, kernel.shape)
+    # print(dist.shape)
+    sdf = sdf.reshape(grid_resolution, grid_resolution, grid_resolution)
+    sdf = sdf.cpu().numpy()
+    return sdf
 
 
 if __name__ == '__main__':
@@ -114,8 +153,10 @@ if __name__ == '__main__':
     scalar_field = np.zeros((grid_resolution, grid_resolution, grid_resolution), dtype=np.float32)
 
     # Compute the scalar field in the grid
-    scalar_field = compute_hoppe(points, normals, scalar_field, grid_resolution, min_grid, size_voxel)
-    # compute_imls(points,normals,scalar_field,grid_resolution,min_grid,size_voxel,30)
+    method = "hoppe"
+    # scalar_field = compute_hoppe(points, normals, scalar_field, grid_resolution, min_grid, size_voxel)
+    method = "imls"
+    scalar_field = compute_imls(points, normals, scalar_field, grid_resolution, min_grid, size_voxel, 30)
 
     # Compute the mesh from the scalar field based on marching cubes algorithm
     verts, faces, normals_tri, values_tri =\
@@ -129,6 +170,6 @@ if __name__ == '__main__':
 
     # Export the mesh in ply using trimesh lib
     mesh = trimesh.Trimesh(vertices=verts, faces=faces)
-    mesh.export(file_obj=outdir/f'bunny_mesh_hoppe_{grid_resolution}.ply', file_type='ply')
+    mesh.export(file_obj=outdir/f'bunny_mesh_{method}_{grid_resolution}.ply', file_type='ply')
 
     print("Total time for surface reconstruction : ", time.time()-t0)
